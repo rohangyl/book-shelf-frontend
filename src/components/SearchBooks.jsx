0f73d6fcac945668f699c31d16f1980ae4b3cbfa
rohangyl <rohana607@gmail.com>
2025-06-06 13:25:23 +0530

    Fix Netlify build

--- src/components/SearchBooks.jsx
+++ src/components/SearchBooks.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback,useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import debounce from 'lodash/debounce';
 import SVGComponent from '../svgfolder/Search';
